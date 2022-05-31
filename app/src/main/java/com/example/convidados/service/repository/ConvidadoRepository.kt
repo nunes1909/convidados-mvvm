16854f1d0aac555d1e2694956e029f06463b5ee1
Gabriel Nunes <80295127+nunes1909@users.noreply.github.com>
2022-05-31 00:19:26 -0300

    Finalizando comportamento de salvar um convidado

--- app/src/main/java/com/example/convidados/service/repository/ConvidadoRepository.kt
+++ app/src/main/java/com/example/convidados/service/repository/ConvidadoRepository.kt
@@ -1,7 +1,12 @@
 package com.example.convidados.service.repository
 
+import android.content.ContentValues
 import android.content.Context
+import android.util.Log
+import com.example.convidados.service.constants.DataBaseConstants
 import com.example.convidados.service.model.ConvidadoModel
+
+private const val TAG = "ConvidadoRepository"
 
 class ConvidadoRepository private constructor(context: Context) {
 
@@ -20,8 +25,21 @@ class ConvidadoRepository private constructor(context: Context) {
     }
 
     // SAVE
-    fun salvar(convidado: ConvidadoModel) {
+    fun salvar(convidado: ConvidadoModel): Boolean {
+        return try {
+            val db = ConvidadoDataBaseHelper.writableDatabase
 
+            val value = ContentValues()
+            value.put(DataBaseConstants.CONVIDADO.COLUMNS.NOME, convidado.nome)
+            value.put(DataBaseConstants.CONVIDADO.COLUMNS.PRESENCA, convidado.presenca)
+
+            db.insert(DataBaseConstants.CONVIDADO.TABLE_NOME, null, value)
+
+            true
+        } catch (e: Exception) {
+            Log.e(TAG, "salvar: ", e)
+            false
+        }
     }
 
     //READ
--- app/src/main/java/com/example/convidados/viewmodel/FormularioViewModel.kt
+++ app/src/main/java/com/example/convidados/viewmodel/FormularioViewModel.kt
@@ -1,15 +1,16 @@
 package com.example.convidados.viewmodel
 
-import android.content.Context
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import com.example.convidados.service.model.ConvidadoModel
 import com.example.convidados.service.repository.ConvidadoRepository
 
-class FormularioViewModel(context: Context) : ViewModel() {
+class FormularioViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val convidadoRepository = ConvidadoRepository.getInstance(context)
+    private val mContext = application.applicationContext
+    private val convidadoRepository = ConvidadoRepository.getInstance(mContext)
 
     private var _salvarConvidado = MutableLiveData<Boolean>()
     val salvarConvidado: LiveData<Boolean> = _salvarConvidado!!
